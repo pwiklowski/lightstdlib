b138d8b5593461cb61ae3c4fa38312ab4b660c05
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-10-04 20:06:39 +0200

    Add loging for esp

--- log.h
+++ log.h
@@ -1,7 +1,14 @@
 #ifndef LOG_H
 #define LOG_H
 
-#define log(line, ...) fprintf(stderr, line, ## __VA_ARGS__ )
 
+#ifdef ESP8266
+
+#include "espressif/esp_common.h"
+#define log(line, ...) printf(line)
+#else
+#include <cstdio>
+#define log(line, ...) fprintf(stderr, line, ## __VA_ARGS__ )
+#endif
 
 #endif // LOG_H
